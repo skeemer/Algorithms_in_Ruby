fe096d9696275895b0130885404a4278febdddde
skeemer <skeemer@gmail.com>
2011-07-06 21:05:39 -0700

    changed argument types to arrays of numbers

--- add.rb
+++ add.rb
@@ -1,20 +1,21 @@
 #This will add two numbers with the same number of digits
 
-def add(in1,in2)
-  n1 = in1.to_s
-  n2 = in2.to_s
-  
-  sum = ""
+def add(n1,n2)
+  sum = []
   carry = 0
   position = n1.length-1
   
   while (position >= 0) do
-    total = n1[position].to_i + n2[position].to_i + carry
-    sum = (total%10).to_s + sum
-    (total>9) ? carry=1 : carry=0
-    position = position-1
+    total = n1[position] + n2[position] + carry
+    sum[position+1] = total%10
+    if (total>9) then
+      carry=1
+    else
+      carry=0
+    end
+    position -= 1
   end
-  sum.to_i
+  sum
 end
 
-puts add(234,531)
+puts add([2,3,4],[5,3,1])
